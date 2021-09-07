8ec1f37fc05160198e5725ed726347695179c054
Jenny Lin <misslinyang91@gmail.com>
2021-09-07 18:23:32 +1000

    created the constructor

--- app.js
+++ app.js
@@ -3,3 +3,43 @@ const canvasContext = canvasEl.getContext("2d");
 
 canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
+
+//runBalls class
+class RunBalls {
+  constructor(xP, yP, xV, yV, radius, red, green, blue) {
+    this.xP = xP;
+    this.yP = yP;
+    this.xV = xV;
+    this.yV = yV;
+    this.radius = radius;
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+  }
+  //drawing the balls
+  drawBalls = function () {
+    canvasContext.fillStyle = `rgb(${this.red}, ${this.green}, ${this.blue})`;
+    canvasContext.beginPath();
+    canvasContext.arc(this.xP, this.yP, this.radius, 0, Math.PI * 2);
+    canvasContext.fill();
+  };
+  //collision detection on edges
+  updateBalls = function () {
+    if (this.xP + this.radius > canvasEl.width || this.xP - this.radius < 0) {
+      this.xV = -this.xV;
+    }
+    if (this.yP + this.radius > canvasEl.height || this.yP - this.radius < 0) {
+      this.yV = -this.yV;
+    }
+
+    this.xP += this.xV;
+  };
+}
+
+//the game loop
+function animatedBalls() {
+  requestAnimationFrame(animatedBalls);
+  canvasContext.clearRect(0, 0, canvasEl.width, canvasEl.height);
+}
+
+animatedBalls();
